12c43aca52712b98a7adfe05cb4b7fbc0271c41c
Adam Hosman <Adam_Hosman@epam.com>
2022-03-18 21:42:40 +0100

    Indent with spaces

--- src/main/java/com/adamhosman/springcloudstreamdemo/SpringCloudStreamDemoApplication.java
+++ src/main/java/com/adamhosman/springcloudstreamdemo/SpringCloudStreamDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCloudStreamDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudStreamDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamDemoApplication.class, args);
+    }
 
 }
