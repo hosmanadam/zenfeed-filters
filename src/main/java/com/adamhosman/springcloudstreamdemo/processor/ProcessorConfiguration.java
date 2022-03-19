a245d1cc7ce27f4fdaa08d450771eb973c101939
Adam Hosman <Adam_Hosman@epam.com>
2022-03-19 20:57:22 +0100

    Add 6-step filter chain

--- src/main/java/com/adamhosman/springcloudstreamdemo/processor/ProcessorConfiguration.java
+++ src/main/java/com/adamhosman/springcloudstreamdemo/processor/ProcessorConfiguration.java
@@ -10,8 +10,33 @@ import java.util.function.Function;
 public class ProcessorConfiguration {
 
     @Bean
-    public Function<String, String> processor() {
-        return (message) -> message + " [PROCESSED]";
+    public Function<String, String> filter1() {
+        return (message) -> message + " #filter1";
+    }
+
+    @Bean
+    public Function<String, String> filter2() {
+        return (message) -> message + " #filter2";
+    }
+
+    @Bean
+    public Function<String, String> filter3() {
+        return (message) -> message + " #filter3";
+    }
+
+    @Bean
+    public Function<String, String> filter4() {
+        return (message) -> message + " #filter4";
+    }
+
+    @Bean
+    public Function<String, String> filter5() {
+        return (message) -> message + " #filter5";
+    }
+
+    @Bean
+    public Function<String, String> filter6() {
+        return (message) -> message + " #filter6";
     }
 
 }
