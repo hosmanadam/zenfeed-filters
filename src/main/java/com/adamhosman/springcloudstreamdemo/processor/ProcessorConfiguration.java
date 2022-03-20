ae10d913aab500c2221bff8cef4915b6b7f786d5
Adam Hosman <Adam_Hosman@epam.com>
2022-03-20 02:25:20 +0100

    3 filters is enough

--- src/main/java/com/adamhosman/springcloudstreamdemo/processor/ProcessorConfiguration.java
+++ src/main/java/com/adamhosman/springcloudstreamdemo/processor/ProcessorConfiguration.java
@@ -13,33 +13,18 @@ import java.util.function.Function;
 public class ProcessorConfiguration {
 
     @Bean
-    public Function<String, String> filter1() {
-        return (message) -> message + " #filter1";
-    }
-
-    @Bean
-    public Function<String, String> filter2() {
-        return (message) -> message + " #filter2";
-    }
-
-    @Bean
-    public Function<String, String> filter3(@Autowired SpamDetectionService service) {
+    public Function<String, String> filter1(@Autowired SpamDetectionService service) {
         return (message) -> service.process(message).orElse(null);
     }
 
     @Bean
-    public Function<String, String> filter4(@Autowired ProfanityDetectionService service) {
+    public Function<String, String> filter2(@Autowired ProfanityDetectionService service) {
         return (message) -> service.process(message).orElse(null);
     }
 
     @Bean
-    public Function<String, String> filter5(@Autowired NegativityDetectionService service) {
+    public Function<String, String> filter3(@Autowired NegativityDetectionService service) {
         return (message) -> service.process(message).orElse(null);
-    }
-
-    @Bean
-    public Function<String, String> filter6() {
-        return (message) -> message + " #filter6";
     }
 
 }
