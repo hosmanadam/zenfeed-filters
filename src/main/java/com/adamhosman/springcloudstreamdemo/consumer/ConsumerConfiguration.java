8042918c825e8cbda87314cc8ab85be9c4b0b7ab
Adam Hosman <Adam_Hosman@epam.com>
2022-03-20 02:06:16 +0100

    Use logger consistently

--- src/main/java/com/adamhosman/springcloudstreamdemo/consumer/ConsumerConfiguration.java
+++ src/main/java/com/adamhosman/springcloudstreamdemo/consumer/ConsumerConfiguration.java
@@ -1,5 +1,7 @@
 package com.adamhosman.springcloudstreamdemo.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +10,11 @@ import java.util.function.Consumer;
 @Component
 public class ConsumerConfiguration {
 
+    private final Logger logger = LoggerFactory.getLogger(ConsumerConfiguration.class);
+
     @Bean
     public Consumer<String> consumer() {
-        return System.out::println;
+        return (message) -> logger.info("### Message received:\n{}", message);
     }
 
 }
