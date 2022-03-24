fed7f14daba9af6b41fc7f386999ad4da7136fdd
Adam Hosman <github@adamsinbox.com>
2022-03-25 00:29:25 +0100

    Add SCS integration tests

--- src/test/java/com/adamhosman/springcloudstreamdemo/SpringCloudStreamDemoApplicationIT.java
+++ src/test/java/com/adamhosman/springcloudstreamdemo/SpringCloudStreamDemoApplicationIT.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringCloudStreamDemoApplicationTests {
+class SpringCloudStreamDemoApplicationIT {
 
     @Test
     void contextLoads() {
---
+++ src/test/java/com/adamhosman/springcloudstreamdemo/processor/filter/ProfanityDetectionServiceIT.java
@@ -0,0 +1,48 @@
+package com.adamhosman.springcloudstreamdemo.processor.filter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.GenericMessage;
+
+import java.util.concurrent.BlockingQueue;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.cloud.stream.test.matcher.MessageQueueMatcher.receivesPayloadThat;
+
+@SpringBootTest
+class ProfanityDetectionServiceIT {
+
+    @Autowired
+    @Qualifier("profanityFilter-in-0")
+    private MessageChannel input;
+
+    @Autowired
+    @Qualifier("profanityFilter-out-0")
+    private MessageChannel output;
+
+    @Autowired
+    private MessageCollector messageCollector;
+    private BlockingQueue<Message<?>> outputCollector;
+
+    @BeforeEach
+    private void setUp() {
+        outputCollector = messageCollector.forChannel(output);
+    }
+
+    @Test
+    void correctsProfanities() {
+        String inputMessage = "This is totally bollocks";
+
+        input.send(new GenericMessage<>(inputMessage));
+
+        assertThat(outputCollector, receivesPayloadThat(is("This is totally unicorns #NoProfanity")));
+    }
+
+}
