2ca541e07f3a3fc80c1acbd01f06ff2811a7a7c5
Adam Hosman <github@adamsinbox.com>
2022-03-26 23:25:29 +0100

    Make the project multi-multi-module

---
+++ zen-filter2/src/test/java/com/adamhosman/springcloudstreamdemo/SpringCloudStreamDemoApplicationIT.java
@@ -0,0 +1,13 @@
+package com.adamhosman.springcloudstreamdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamDemoApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
