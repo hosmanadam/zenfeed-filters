a40942268cf9929baa1410c3236cb99f7396a2dc
Adam Hosman <github@adamsinbox.com>
2022-03-28 19:04:55 +0200

    Implement integration tests

--- filters/profanity-filter/src/test/java/com/zenfeed/filters/profanity/ProfanityFilterApplicationIT.java
+++ filters/profanity-filter/src/test/java/com/zenfeed/filters/profanity/ProfanityFilterApplicationIT.java
@@ -1,35 +1,35 @@
-//package com.zenfeed.filters.profanity;
-//
-//import com.zenfeed.filters.profanity.messaging.ProfanityListener;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//
-//import static com.zenfeed.filters.profanity.messaging.MessagingConfiguration.TOPIC_OUT;
-//
-//@SpringBootTest
-//class ProfanityFilterApplicationIT {
-//
-//    @Autowired
-//    private ProfanityListener profanityListener;
-//
-//    @MockBean
-//    private RabbitTemplate rabbitTemplate;
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//    @Test
-//    void correctsProfanities() {
-//        String inputMessage = "This is totally bollocks";
-//
-//        profanityListener.consume(inputMessage);
-//
-//        Mockito.verify(rabbitTemplate).convertAndSend(TOPIC_OUT, "This is totally unicorns #NoProfanity");
-//    }
-//
-//}
+package com.zenfeed.filters.profanity;
+
+import com.zenfeed.filters.profanity.messaging.ProfanityListener;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import static com.zenfeed.filters.profanity.messaging.MessagingConfiguration.TOPIC_OUT;
+
+@SpringBootTest
+class ProfanityFilterApplicationIT {
+
+    @Autowired
+    private ProfanityListener profanityListener;
+
+    @MockBean
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void correctsProfanities() {
+        String inputMessage = "This is totally bollocks";
+
+        profanityListener.consume(inputMessage);
+
+        Mockito.verify(kafkaTemplate).send(TOPIC_OUT, "This is totally unicorns #NoProfanity");
+    }
+
+}
--- testing/rest-producer/src/test/java/com/zenfeed/filters/testing/restproducer/RestProducerIT.java
+++ testing/rest-producer/src/test/java/com/zenfeed/filters/testing/restproducer/RestProducerIT.java
@@ -1,38 +1,38 @@
-//package com.zenfeed.filters.testing.restproducer;
-//
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static com.zenfeed.filters.testing.restproducer.configuration.MessagingConfiguration.TOPIC_OUT;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//class RestProducerIT {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private RabbitTemplate rabbitTemplate;
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//    @Test
-//    void producesMessage() throws Exception {
-//        String requestBody = "Could you send this message please?";
-//
-//        mockMvc.perform(post("/produce").content(requestBody));
-//
-//        Mockito.verify(rabbitTemplate).convertAndSend(TOPIC_OUT, requestBody);
-//    }
-//
-//}
+package com.zenfeed.filters.testing.restproducer;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.zenfeed.filters.testing.restproducer.configuration.MessagingConfiguration.TOPIC_OUT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RestProducerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void producesMessage() throws Exception {
+        String requestBody = "Could you send this message please?";
+
+        mockMvc.perform(post("/produce").content(requestBody));
+
+        Mockito.verify(kafkaTemplate).send(TOPIC_OUT, requestBody);
+    }
+
+}
